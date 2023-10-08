7803e1fda5a8c77dc1a73705de3a497a863e202f
VuslatYolcu <ylcvalt@gmail.com>
2023-10-08 19:01:55 +0300

    Add combine example

--- CombineIntro/ViewController.swift
+++ CombineIntro/ViewController.swift
@@ -9,7 +9,34 @@ import UIKit
 import Combine
 
 class MyCustomViewCell: UITableViewCell {
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemPink
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
     
+    let action = PassthroughSubject<String, Never>()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+
+    @objc private func didTapButton() {
+        action.send("Cool button was tapped!")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        button.frame = CGRect(x: 10, y: 3, width: contentView.frame.size.width - 20 , height: contentView.frame.size.height - 6)
+    }
 }
 
 class ViewController: UIViewController {
@@ -20,8 +47,8 @@ class ViewController: UIViewController {
         return tableView
     }()
     
-    // Comes with Combine Framework
-    var observer: AnyCancellable?
+    // AnyCancellable comes with Combine Framework, so you need to import it
+    var observers: [AnyCancellable] = []
     private var models = [String]()
     
     override func viewDidLoad() {
@@ -36,7 +63,7 @@ class ViewController: UIViewController {
 
     
     private func getCompanies() {
-        observer = APICaller.shared.fetchCompanies()
+        APICaller.shared.fetchCompanies()
             .receive(on: DispatchQueue.main) // Main threadde almak istediğini söylüyorsun
             .sink(receiveCompletion: { completion in
             switch completion {
@@ -48,7 +75,7 @@ class ViewController: UIViewController {
         }, receiveValue: { [weak self] value in
             self?.models = value
             self?.tableView.reloadData()
-        })
+        }).store(in: &observers)
     }
 
 }
@@ -58,7 +85,11 @@ extension ViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCustomViewCell else {
             fatalError()
         }
-        cell.textLabel?.text = models[indexPath.row]
+        
+        cell.action.sink { string in
+            print(string)
+        }.store(in: &observers)
+        
         return cell
     }
     
